0f119fb41c7c6cc95b1d5fcf57ba2aba4d9b6e25
Hafiz Muhammad Anees <mascyberix@gmail.com>
2025-09-28 10:48:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.json({ limit: '30kb' }));
     }
   });
 
-  const ALLOWED_ORIGINS = Array.from(aug.length ? aug : ['https://apextechagency.com', 'https://www.apextechagency.com']);
+  const ALLOWED_ORIGINS = Array.from(aug.length ? aug : ['https://apextechagency.com', 'https://www.apextechagency.com', 'https://tools-api.apextechagency.com']);
   // flexible CORS: allow unknown origins only if ALLOWED_ORIGINS contains '*'
   app.use(cors({
     origin: function (origin, callback) {
